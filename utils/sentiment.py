dec66b2da5b82ca8f8125754164db3316c09c985
jyaacoub <jyaacoub21@gmail.com>
2022-10-22 11:34:30 -0400

    sentiment base
    I did some research I think pretrained models would be the best way to go here
    Will be testing a couple of them to see which is best for our situation

---
+++ utils/sentiment.py
@@ -0,0 +1,12 @@
+"""
+A couple pre-trained sentiment analysis models that I will test:
+* Vader
+* TextBlob
+* Happy Transformer
+
+I have tested/explored:
+* AFINN
+* SentiWordNet
+* SentiStrength
+"""
+
