0912c66df326461166b426d7725a9d49eb1d7a67
jyaacoub <jyaacoub21@gmail.com>
2022-11-10 22:07:55 -0500

    psudocode for monte carlo method
    Need to set up datastructure -> O(n)
    best so far is O(b*n^2) time for all probabilities

--- run.py
+++ run.py
@@ -32,7 +32,52 @@ for chunk in b_reader:
     if len(b_ids) >= CUTTOFF: # cut off
         break
     
-# %% Iterate through the reviews and create dictionary of user ids and their reviews
+# %% Iterate through the reviews and create dictionary of user ids and their network
+
+####### METHOD 1: iterating through businesses first #######
+# USERS:      {ID: (friends_ID)} # friends_ID is a set of user ids for constant time lookup
+# BUSINESSES: {ID: (Usr_ID)}  # Usr_ID is also a set
+
+# Assuming we created the data structure above we now preform the following:
+#    Note that this will miss P(User writes a review | 0 friends wrote a review)?
+#        ^ This isn't actually true but I feel like Im still missing something related to this
+prob_counts = {} # keeps track of instances of (User writes a review | i friend(s) wrote a review) where i is the key
+for usr_ids in BUSINESSES:
+    for id in usr_ids:
+        num_rev = 0
+        for friend_id in USERS[id]:
+            if friend_id in usr_ids:
+                num_rev += 1
+        
+        # add to the probabilities dictionary or create it
+        if num_rev in prob_counts:
+            prob_counts[num_rev] += 1
+        else:
+            prob_counts[num_rev] = 1
+
+# in the worst case everyone is friends with everyone and so this would run in O(b*n^2) 
+# where n is the number of users and b is the number of businesses
+    
+####### METHOD 2: iterating through users first #######
+# USERS:    {ID: {"Network": (friends_ID), 
+#               "Reviews": (business_ID)}
+#           }
+
+for usr in USERS:
+    for b in usr["Reviews"]:
+        num_rev = 0
+        for friend_id in usr["Network"]:
+            if b in USERS[friend_id]["Reviews"]: # constant time lookup
+                num_rev += 1
+        
+        # add to the probabilities dictionary or create it
+        if num_rev in prob_counts:
+            prob_counts[num_rev] += 1
+        else:
+            prob_counts[num_rev] = 1
+
+# this should also run in O(b*n) 
+# will use this to validate that i havent missed anything in the first method
 
 # iterate through users and put them into a dictionary with the key being the user id
     # and the value being another dict of business ids that they have reviewed
