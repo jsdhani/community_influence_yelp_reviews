2b5e5e47de0cbb9478c3f7f43bf7c7949a63cf16
jyaacoub <50300488+jyaacoub@users.noreply.github.com>
2022-11-22 13:21:20 -0500

    binned MC probs

--- run.py
+++ run.py
@@ -17,6 +17,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import pickle
+import math
 
 
 # %%
@@ -30,13 +31,65 @@ for x in range(10):
     
 PKL_FOLDER_PATH = f"{RESULTS}/monte_carlo_prob0/"
 
+#%%
+def bin_data(data, bins=[x for x in range(0,51,5)], ignore_exact=[0,1]):
+    """
+    This function is binning the data into bins number of bins.
+    """
+    binned_d = {k: 0 for k in bins}
+    for i in range(len(bins)):
+        l = bins[i]
+        r = bins[i+1] if i < len(bins)-1 else math.inf # last bin is open ended
+        for k,v in data.items():
+            if ignore_exact and k in ignore_exact: continue
+            if l <= k and k <= r:
+                binned_d[l] += v
+    return binned_d
+
+def plot_bins(binned_d0, binned_d1=None):
+    if binned_d1 == None:
+        data = np.array([[x,y] for x,y in binned_d0.items()])
+        plt.bar(data[:,0], data[:,1], width=5, align='edge')
+    else: # grouped bar chart
+        data_0 = np.array([[x,y] for x,y in binned_d0.items()])
+        data_1 = np.array([[x,y] for x,y in binned_d1.items()])
+        
+        h0 = plt.bar(data_0[:,0], data_0[:,1], width=2.5, align='edge')
+        h1 = plt.bar(data_1[:,0]+2.5, data_1[:,1], width=2.5, align='edge')
+        
+        plt.legend((h0[0], h1[0]), ('P(0|i)', 'P(1|i)'))
+
+# %%
+MC_PATH_PKL = "results/monte_carlo_prob0/"
+MC_PATH_MEDIA = "media/monte_carlo_prob01_binned/"
+for t in time_periods[0:5]:
+    t_s = f"{t[0].strftime('%Y-%m-%d')}_{t[1].strftime('%Y-%m-%d')}"
+    curr_path = lambda x: f"{MC_PATH_PKL}{t_s}_prob_{x}.pkl"
+    
+    data_0 = pickle.load(open(curr_path(0), "rb"))
+    data_1 = pickle.load(open(curr_path(1), "rb"))
+    
+    bins = [x for x in range(0,51,5)]
+    ignore_exact = [0]
+    bd_0 = bin_data(data_0, bins, ignore_exact)
+    bd_1 = bin_data(data_1, bins, ignore_exact)
+    plot_bins(bd_1, bd_0)
+    plt.xlabel("Number of i friends who reviewed same business")
+    plt.ylabel("Monte Carlo probability")
+    plt.title(f"{t_s}: Ignoring i={ignore_exact}")
+    i_str = "".join([str(x) for x in ignore_exact])
+    plt.savefig(f"{MC_PATH_MEDIA}{t_s}_prob_01_i{i_str}.png")
+    plt.show()
+    plt.clf()
+    
+    
+    
 
 # %% monte carlo
-for t in time_periods:
-    rp = ReviewProb(save_path=PKL_FOLDER_PATH)
-    rp.prep_data_range(date_range=(t[0], t[1]))
-    rp.get_probs(plot=False, save=True)
-    
+# for t in time_periods:
+#     rp = ReviewProb(save_path=PKL_FOLDER_PATH)
+#     rp.prep_data_range(date_range=(t[0], t[1]))
+#     rp.get_probs(plot=False, save=True)
 
 # # %% getting data
 # for t in time_periods:
@@ -70,10 +123,18 @@ for t in time_periods:
     
     data = data_0
     
-    pear = get_pearson(data, alt="two-sided")
-    line = get_linear_reg(data)
+    pear_0 = get_pearson(data_0, alt="two-sided")
+    line_0 = get_linear_reg(data_0)
     
-    pears.append((pear.statistic, pear.pvalue))
+    pear_1 = get_pearson(data_1, alt="two-sided")
+    line_1 = get_linear_reg(data_1)
+    
+    pear = (pear_1[0],#-pear_1[0], 
+            pear_1[1])#-pear_1[1])
+    line = (line_1[0],#-line_1[0], 
+            line_1[1])#-line_1[1])
+    
+    pears.append(pear)
     lines.append(line)
     
     print("{:25}|{:^10.3}|{:^10.3}|{:^10.3}|{:^10.3}".format(t_s, pear[0], pear[1], line[0], line[1]))
